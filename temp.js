416c823faf42dbbb3439452f618abefd274727f2
ybliumit <yuanbo@mit.edu>
2020-11-17 11:39:20 -0500

    1

--- temp.js
+++ temp.js
@@ -27,3 +27,31 @@ async function getEmoji(){
 }
 
 getEmoji()
+
+async function getEmoji(){
+  var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+  };
+  const url= "https://api.ritekit.com/v1/emoji/suggestions?text=Why%20robots%20may%20soon%20steal%20all%20manufacturing%20jobs%20%E2%80%93%20but%20it%E2%80%99s%20not%20all%20bad%20news&client_id=8c9c3bdd0dd47815a8eebef91b9711a042c28c7a2b1d";
+  const response = await fetch(url, requestOptions)
+  //.then(response => response.text())
+  //.then(result => console.log(result))
+  //.catch(error => console.log('error', error));
+const json = await response.json();
+console.log(json)
+return json.data
+}
+
+getEmoji()
+
+console.log('about to fetch something...');
+fetch('rainbow.jpg')
+    .then(response => {
+        console.log(response);
+        return response.blob();
+    })
+    .then(blob=>{
+        console.log(blob);
+        document.getElementById('rainbow').src = URL.createObjectURL(blob);
+    })
